d49ad1543249265556c39f5c2462fa212b71bf71
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-09 19:07:21 -0600

    refactor(api): organize app module

--- backend/infracciones-api/src/app.module.ts
+++ backend/infracciones-api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import { DatabaseModule } from './database/database.module';
+import { CatalogosModule } from './modules/catalogos/catalogos.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    DatabaseModule,
+    RolesModule,
+    UsuariosModule,
+    CatalogosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
