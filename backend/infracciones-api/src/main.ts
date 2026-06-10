9487156ebe8c67c217c72e763ccc06c7cda38641
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-10 08:14:55 -0600

    feat: enable global validation pipe

--- backend/infracciones-api/src/main.ts
+++ backend/infracciones-api/src/main.ts
@@ -1,5 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
@@ -7,6 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('app.port', 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(port);
 }
