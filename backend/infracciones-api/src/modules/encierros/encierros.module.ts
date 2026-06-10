a8d40dc0a98994b1dc2faeb972804685d76515ea
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-09 23:13:24 -0600

    feat: register encierros service

--- backend/infracciones-api/src/modules/encierros/encierros.module.ts
+++ backend/infracciones-api/src/modules/encierros/encierros.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { EncierrosService } from './encierros.service';
 import { Encierro } from './entities/encierro.entity';
 import { RetencionVehiculo } from './entities/retencion-vehiculo.entity';
 import { SalidaVehiculo } from './entities/salida-vehiculo.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Encierro, RetencionVehiculo, SalidaVehiculo])],
+  providers: [EncierrosService],
+  exports: [EncierrosService],
 })
 export class EncierrosModule {}
