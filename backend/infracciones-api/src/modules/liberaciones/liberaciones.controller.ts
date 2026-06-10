5601f45333146fce3902990dcff508cff76c8737
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-09 23:30:11 -0600

    feat: add liberaciones controller base

---
+++ backend/infracciones-api/src/modules/liberaciones/liberaciones.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+
+import { GenerarLiberacionDto } from './dto/generar-liberacion.dto';
+import { LiberacionesService } from './liberaciones.service';
+
+@Controller('liberaciones')
+export class LiberacionesController {
+  constructor(private readonly liberacionesService: LiberacionesService) {}
+
+  @Get(':idLiberacionVehiculo')
+  findById(@Param('idLiberacionVehiculo', ParseIntPipe) idLiberacionVehiculo: number) {
+    return this.liberacionesService.findByIdOrFail(idLiberacionVehiculo);
+  }
+
+  @Get('infraccion/:idInfraccion')
+  findByInfraccion(@Param('idInfraccion', ParseIntPipe) idInfraccion: number) {
+    return this.liberacionesService.findByInfraccion(idInfraccion);
+  }
+
+  @Post()
+  generarLiberacion(@Body() generarLiberacionDto: GenerarLiberacionDto) {
+    return this.liberacionesService.generarLiberacion(generarLiberacionDto);
+  }
+}
