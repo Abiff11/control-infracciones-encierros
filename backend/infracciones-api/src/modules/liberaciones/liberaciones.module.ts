a2dbf23fd2668e3fcdc7c9b04c45d9431c071fb1
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-09 23:01:39 -0600

    feat: register vehicle release service

--- backend/infracciones-api/src/modules/liberaciones/liberaciones.module.ts
+++ backend/infracciones-api/src/modules/liberaciones/liberaciones.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { LiberacionVehiculo } from './entities/liberacion-vehiculo.entity';
+import { LiberacionesService } from './liberaciones.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([LiberacionVehiculo])],
+  providers: [LiberacionesService],
+  exports: [LiberacionesService],
 })
 export class LiberacionesModule {}
