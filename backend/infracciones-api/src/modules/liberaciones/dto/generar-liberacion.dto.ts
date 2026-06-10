81d72feb0b69fb110d525748ea8d5107dbb4f9b6
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-10 08:16:52 -0600

    feat: validate generar liberacion dto

--- backend/infracciones-api/src/modules/liberaciones/dto/generar-liberacion.dto.ts
+++ backend/infracciones-api/src/modules/liberaciones/dto/generar-liberacion.dto.ts
@@ -1,10 +1,32 @@
+import { IsDate, IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class GenerarLiberacionDto {
+  @IsInt()
   idInfraccion!: number;
+
+  @IsInt()
   idPagoInfraccion!: number;
+
+  @IsInt()
   idUsuarioLibera!: number;
+
+  @IsString()
+  @IsNotEmpty()
   folioLiberacion!: string;
+
+  @IsString()
+  @IsNotEmpty()
   liberadoPor!: string;
+
+  @IsString()
+  @IsNotEmpty()
   nombreRecibeLiberacion!: string;
+
+  @IsOptional()
+  @IsDate()
   fechaLiberacion?: Date;
+
+  @IsOptional()
+  @IsString()
   observacion?: string | null;
 }
