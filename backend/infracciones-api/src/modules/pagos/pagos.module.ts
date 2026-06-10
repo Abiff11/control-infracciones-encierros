c12d584e307a2325466196402f1d35642a3663e7
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-09 23:31:32 -0600

    feat: register pagos controller

--- backend/infracciones-api/src/modules/pagos/pagos.module.ts
+++ backend/infracciones-api/src/modules/pagos/pagos.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PagoInfraccion } from './entities/pago-infraccion.entity';
+import { PagosController } from './pagos.controller';
 import { PagosService } from './pagos.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PagoInfraccion])],
+  controllers: [PagosController],
   providers: [PagosService],
   exports: [PagosService],
 })
