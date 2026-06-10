263ac7fa9b6bbd16c207178068eb62373f34a34a
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-09 23:30:34 -0600

    feat: add encierros controller base

---
+++ backend/infracciones-api/src/modules/encierros/encierros.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+
+import { RegistrarRetencionDto } from './dto/registrar-retencion.dto';
+import { RegistrarSalidaDto } from './dto/registrar-salida.dto';
+import { EncierrosService } from './encierros.service';
+
+@Controller('encierros')
+export class EncierrosController {
+  constructor(private readonly encierrosService: EncierrosService) {}
+
+  @Get(':idEncierro')
+  findEncierroById(@Param('idEncierro', ParseIntPipe) idEncierro: number) {
+    return this.encierrosService.findEncierroByIdOrFail(idEncierro);
+  }
+
+  @Get('retenciones/:idRetencionVehiculo')
+  findRetencionById(@Param('idRetencionVehiculo', ParseIntPipe) idRetencionVehiculo: number) {
+    return this.encierrosService.findRetencionByIdOrFail(idRetencionVehiculo);
+  }
+
+  @Post('retenciones')
+  registrarRetencion(@Body() registrarRetencionDto: RegistrarRetencionDto) {
+    return this.encierrosService.registrarRetencion(registrarRetencionDto);
+  }
+
+  @Post('salidas')
+  registrarSalida(@Body() registrarSalidaDto: RegistrarSalidaDto) {
+    return this.encierrosService.registrarSalida(registrarSalidaDto);
+  }
+}
