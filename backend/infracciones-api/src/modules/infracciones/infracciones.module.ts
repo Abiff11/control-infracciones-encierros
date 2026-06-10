40bdef633ec4d49e602180c21e2af4d863db49b9
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-09 22:20:39 -0600

    feat: register infraccion movimiento entity

--- backend/infracciones-api/src/modules/infracciones/infracciones.module.ts
+++ backend/infracciones-api/src/modules/infracciones/infracciones.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { InfraccionMotivo } from './entities/infraccion-motivo.entity';
+import { InfraccionMovimiento } from './entities/infraccion-movimiento.entity';
 import { Infraccion } from './entities/infraccion.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Infraccion, InfraccionMotivo])],
+  imports: [TypeOrmModule.forFeature([Infraccion, InfraccionMotivo, InfraccionMovimiento])],
 })
 export class InfraccionesModule {}
