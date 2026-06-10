c49be76f2b41dc2f5df0ee703f43b9a0429fcca8
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-09 22:08:37 -0600

    feat: register salida vehiculo entity

--- backend/infracciones-api/src/modules/encierros/encierros.module.ts
+++ backend/infracciones-api/src/modules/encierros/encierros.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Encierro } from './entities/encierro.entity';
 import { RetencionVehiculo } from './entities/retencion-vehiculo.entity';
+import { SalidaVehiculo } from './entities/salida-vehiculo.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Encierro, RetencionVehiculo])],
+  imports: [TypeOrmModule.forFeature([Encierro, RetencionVehiculo, SalidaVehiculo])],
 })
 export class EncierrosModule {}
