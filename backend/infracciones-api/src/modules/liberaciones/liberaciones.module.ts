7ad17cdccfde2c214d17d99d34549cdcef3defad
Hiram Carreño Armenta <143430604+Abiff11@users.noreply.github.com>
2026-08-10 08:26:22 -0600

    fix: registrar repositorio de retenciones en liberaciones

--- backend/infracciones-api/src/modules/liberaciones/liberaciones.module.ts
+++ backend/infracciones-api/src/modules/liberaciones/liberaciones.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuditoriaModule } from '../auditoria/auditoria.module';
+import { RetencionVehiculo } from '../encierros/entities/retencion-vehiculo.entity';
 import { InfraccionesModule } from '../infracciones/infracciones.module';
 import { LiberacionVehiculo } from './entities/liberacion-vehiculo.entity';
 import { LiberacionesController } from './liberaciones.controller';
@@ -9,7 +10,7 @@ import { LiberacionesService } from './liberaciones.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([LiberacionVehiculo]),
+    TypeOrmModule.forFeature([LiberacionVehiculo, RetencionVehiculo]),
     InfraccionesModule,
     AuditoriaModule,
   ],
