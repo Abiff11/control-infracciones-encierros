18f6fd7d738f43add260a66834c55e4c8a6712b0
Hiram Carreño Armenta <143430604+Abiff11@users.noreply.github.com>
2026-08-10 10:04:22 -0600

    feat: registrar entidades de conceptos de pago

--- backend/infracciones-api/src/modules/pagos/pagos.module.ts
+++ backend/infracciones-api/src/modules/pagos/pagos.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuditoriaModule } from '../auditoria/auditoria.module';
-import { RetencionVehiculo } from '../encierros/entities/retencion-vehiculo.entity';
 import { InfraccionesModule } from '../infracciones/infracciones.module';
+import { ConceptoPago } from './entities/concepto-pago.entity';
+import { PagoConcepto } from './entities/pago-concepto.entity';
 import { PagoInfraccion } from './entities/pago-infraccion.entity';
 import { PagosController } from './pagos.controller';
 import { PagosService } from './pagos.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([PagoInfraccion, RetencionVehiculo]),
+    TypeOrmModule.forFeature([PagoInfraccion, ConceptoPago, PagoConcepto]),
     InfraccionesModule,
     AuditoriaModule,
   ],
