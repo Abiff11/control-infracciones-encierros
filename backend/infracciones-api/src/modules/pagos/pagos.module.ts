8240afc9d56d07c3462e1dc4b62ff9db76050d76
Hiram Carreño Armenta <143430604+Abiff11@users.noreply.github.com>
2026-08-10 08:26:12 -0600

    fix: registrar repositorio de retenciones en pagos

--- backend/infracciones-api/src/modules/pagos/pagos.module.ts
+++ backend/infracciones-api/src/modules/pagos/pagos.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuditoriaModule } from '../auditoria/auditoria.module';
+import { RetencionVehiculo } from '../encierros/entities/retencion-vehiculo.entity';
 import { InfraccionesModule } from '../infracciones/infracciones.module';
 import { PagoInfraccion } from './entities/pago-infraccion.entity';
 import { PagosController } from './pagos.controller';
@@ -9,7 +10,7 @@ import { PagosService } from './pagos.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([PagoInfraccion]),
+    TypeOrmModule.forFeature([PagoInfraccion, RetencionVehiculo]),
     InfraccionesModule,
     AuditoriaModule,
   ],
